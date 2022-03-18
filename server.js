2b52ed20751e048d21cec685a54088205ee1cc07
nickolas <nicklops8@gmail.com>
2022-03-18 18:38:35 -0300

    nodemon adicionado no projeto

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require("http");
 const port = 3000;
 
+const rotas = {
+    '/': "Curso de Node",
+    '/livros': "Entrei na página de livros",
+    "/autores": "Listagem de autores",
+    "/editora": "Págian de editora",
+    "/sobre": "Informações sobre o projeto"
+};
+    
 const server = http.createServer((req,res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Curso de Node');
-})
+    res.end(rotas[req.url]);
+});
 
 server.listen(port, () => {
     console.log(`Server is running on port: http://localhost:${port}`);
