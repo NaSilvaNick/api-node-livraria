506ceefdc4408d96c690b9794be5aec300bb83a5
nickolas <nicklops8@gmail.com>
2022-03-18 22:17:11 -0300

    Metodos de PUT e GET by id criados

--- src/app.js
+++ src/app.js
@@ -7,20 +7,32 @@ app.use(express.json());
 const livros = [
     {id: 1, "titulo" : "Senhor dos Aneis"},
     {id: 2, "titulo" : "O Hobbit"}
-]
+];
 
 app.get("/", (req,res) => {
     res.status(200).send("Curso de Node");
 });
 
-
 app.get("/livros", (req,res) => {
     res.status(200).send(livros);
+});
+
+app.get("/livros/:id", (req,res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
 });
 
 app.post("/livros", (req,res) => {
     livros.push(req.body);
     res.status(200).send("livro cadastrado com sucesso!");
 });
+
+app.put("/livros/:id", (req,res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+});
+
+const buscaLivro = id => livros.findIndex(livro => livro.id == id);
 
 export default app;
