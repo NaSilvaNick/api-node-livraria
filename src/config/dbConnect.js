4c2fb3ace2fa74d5526f98683a51664264b7b5c6
nickolas <nicklops8@gmail.com>
2022-03-19 21:07:33 -0300

    Fixing URL do banco de dados

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://NaSilvaNick:NickGab011120@api-node-livraria.uwteu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://NaSilvaNick:NickGab011120@api-node-livraria.uwteu.mongodb.net/api-node-livraria");
 
 const db = mongoose.connection;
 
