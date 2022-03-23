066207b15e34ad681156a6c19b8822c8aff35d6a
nickolas <nicklops8@gmail.com>
2022-03-22 21:02:37 -0300

    Limpando rascunhos do Server.js

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ import app from "./src/app.js"
 
 const port = process.env.PORT || 3000;
 
-const rotas = {
-    '/': "Curso de Node",
-    '/livros': "Entrei na página de livros",
-    "/autores": "Listagem de autores",
-    "/editora": "Págian de editora",
-    "/sobre": "Informações sobre o projeto"
-};
-
 app.listen(port, () => {
     console.log(`Server is running on port: http://localhost:${port}`);
 });
